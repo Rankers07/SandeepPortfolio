b264900cb95c0e3655343e5915cf96c8b9089e89
Rankers07 <sgupta73853@gmail.com>
2026-03-15 23:21:56 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,279 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/uptricks.png';
+import agcLogo from './assets/company_logo/grastech.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/doctorLogo.png';
+import csprepLogo from './assets/work_logo/Codepractice.png';
+import movierecLogo from './assets/work_logo/sidcup.png';
+import taskremLogo from './assets/work_logo/Billing.png';
+import taskremLog from './assets/work_logo/game.png';
+import webverLogo from './assets/work_logo/calculator.png';
+import cmLogo from './assets/work_logo/instalove.png';
+import imagesearchLogo from './assets/work_logo/multiplehover.png';
+import removebgLogo from './assets/work_logo/friendfeature.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "MERN Stack Developer Intern",
+      company: "Uptricks Pvt. Ltd.",
+      date: "April 2025 - October 2025",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux"
+       
+      ],
+    },
+    {
+      id: 1,
+      img: agcLogo,
+      role: "Fullstack Developer Intern",
+      company: "GRAStech",
+      date: "February 2025 - April 2025",
+      desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, SQL, Tailwind CSS, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "SQL",
+      ],
+     },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: glaLogo,
+      school: "Babu Banarasi Das University, Lucknow",
+      date: "Sept 2022 - July 2026",
+      grade: "8.92 CGPA",
+      desc: "I'm persuing my Batchelor degree (BTech) in Computer Science & Engineering from Babu Banarasi Das University, Lucknow. During my time at BBDU, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "Batchelor Degree - BTech",
+    },
+    {
+      id: 1,
+      img: bsaLogo,
+      school: "Masih Sevasaram Inter College, Nichlaul Maharajganj",
+      date: "Sept 2019 - july 2020",
+      grade: "74.60%",
+      desc: "I completed my class 12 education from Masih Sevasaram Inter College, Nichlaul, under the State board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "State Board(XII) - PCM",
+    },
+    {
+      id: 2,
+      img: bsaLogo,
+      school: "Masih Sevasaram High School Nichlaul Maharajganj",
+      date: "Apr 2017 - March 2018",
+      grade: "82.50%",
+      desc: "I completed my class 10 education from Masih Sevasaram High School Nichlaul Maharajganj, under the Up board.",
+      degree: "State Board(X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Doctor Appointment System",
+      description:
+        "Developed a full-stack Doctor Appointment System using the MERN stack for patient–doctor scheduling and appointment management. Implemented authentication and role-based access using Node.js and Express.js. Built responsive interfaces with React and Tailwind CSS and created RESTful APIs for appointment handling. Integrated Cloudinary for document storage and optimized backend performance.",
+      image: githubdetLogo,
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "RESTfull APIs", "Node JS", "Express JS", "MongoDB", "WebSocket"],
+      github: "https://github.com/Rankers07/Prescripto",
+      webapp: "",
+    },
+    {
+      id: 1,
+      title: "Smart Coding Arena For Solve the Coding Problem",
+      description:
+        "Developed an Intelligent Code Practice Platform using the MERN stack for solving coding problems. Implemented role-based authentication, AI-based hints, and real-time video communication. Used Redis caching and API rate limiting to improve backend performance.",
+
+      image: csprepLogo,
+      tags: ["HTML", "CSS", "JavaScript", "MERN Stack", "WebSocket", "redis", "Judge0",  "Cloudinary"],
+      github: "",
+      webapp: "",
+    },
+    {
+      id: 2,
+      title: "LSidcup Family Golf Animated Website",
+      description:
+        "Developed an animated website inspired by the Sidcup Family Golf design using HTML, CSS, and JavaScript. Implemented smooth scroll animations, hover effects, and interactive UI elements to enhance user engagement. Designed a fully responsive layout to ensure compatibility across different devices and screen sizes.",
+      image: movierecLogo,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Rankers07/LSidcupFamilyGolfAnimatedWebsite",
+      webapp: "https://sweet-churros-28b8be.netlify.app/",
+    },
+    {
+      id: 3,
+      title: "Billing Management System",
+      description:
+        "Developed a Billing Management System frontend using HTML, CSS, and JavaScript to manage billing operations efficiently. Designed responsive and user-friendly interfaces for creating, viewing, and managing invoices. Implemented interactive UI components and form validations to improve usability and ensure a smooth user experience.",
+       image: taskremLogo,
+        tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Rankers07/-billingManagementSystem",
+      webapp: " https://billingmanagementsystemcom.netlify.app/",
+    },
+    {
+      id: 4,
+      title: "Game Changer Animated Website",
+      description:
+        "Developed an animated website using HTML, CSS, and JavaScript featuring smooth transitions and interactive UI effects. Implemented scroll-based animations, hover effects, and dynamic elements to enhance user engagement. Designed a responsive layout to ensure compatibility across various devices and screen sizes.",
+
+      image: taskremLog,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Rankers07/gameChangerAnimatedWebsite",
+      webapp: "https://animatedwebsitecom.netlify.app/",
+    },
+    {
+      id: 5,
+      title: "Modern Calculator ",
+      description:
+        "Developed a Modern Calculator using HTML, CSS, and JavaScript to perform basic arithmetic operations. Implemented an interactive and user-friendly interface with responsive design. Utilized JavaScript event handling and DOM manipulation to handle calculations efficiently.",
+      image: webverLogo,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Rankers07/ModernCalculator",
+      webapp: " https://moderneracalculators.netlify.app/ ",
+    },
+    {
+      id: 6,
+      title: " Insta Love Button Website ",
+      description:
+        "Developed an interactive Insta Love Button website using HTML, CSS, and JavaScript. Implemented dynamic like button animations and hover effects to enhance user interaction. Designed a responsive and visually appealing interface for a smooth user experience.",
+      image: cmLogo,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Rankers07/InstaLoveButtonWebsite",
+      webapp: "https://instalovebuttonwebsite.netlify.app/",
+    },
+    {
+      id: 7,
+      title: "Hover Multiple Website",
+      description:
+        "Developed a Hover Multiple Website using HTML, CSS, and JavaScript to showcase interactive hover-based UI effects. Implemented multiple hover animations and transitions to enhance visual engagement. Designed a responsive layout ensuring smooth interaction across different devices.",
+      image: imagesearchLogo,
+      tags: ["HTML", "CSS", "Javascript"],
+      github: "https://github.com/Rankers07/HoverMultipleWebsite",
+      webapp: "https://hovermultiplewebsite.netlify.app/ ",
+    },
+    {
+      id: 8,
+      title: "Friend Feature Website",
+      description:
+        "Developed a Friend Feature Website using HTML, CSS, and JavaScript to simulate adding and removing friends through interactive buttons. Implemented dynamic UI updates and click-based interactions using JavaScript DOM manipulation. Designed a responsive and user-friendly interface to enhance user engagement.",
+
+      image:removebgLogo,
+      tags : ["HTML", "CSS", "Javascript"],
+      github: "https://github.com/Rankers07/FriendFeatureWebsite",
+      webapp: "https://friendfeaturewebsite.netlify.app/",
+    },
+  ];  
